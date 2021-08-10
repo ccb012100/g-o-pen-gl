f205735dbf2f88dcdcf649d4077c4a3a777c2764
Christopher Bocardo <ccb012100@users.noreply.github.com>
2021-08-10 17:37:45 -0500

    creates a window

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/ccb012100/g-o-pen-gl
+
+go 1.16
+
+require (
+	github.com/go-gl/gl v0.0.0-20210501111010-69f74958bac0 // indirect
+	github.com/go-gl/glfw/v3.3/glfw v0.0.0-20210727001814-0db043d8d5be // indirect
+)
---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"log"
+	"runtime"
+
+	"github.com/go-gl/gl/v4.6-core/gl"
+	"github.com/go-gl/glfw/v3.3/glfw"
+)
+
+func init() {
+	// This is needed to arrange that main() runs on main thread.
+	// See documentation for functions that are only allowed to be called from the main thread.
+	runtime.LockOSThread()
+}
+
+func main() {
+	err := glfw.Init()
+	if err != nil {
+		panic(err)
+	}
+	defer glfw.Terminate()
+
+	window, err := glfw.CreateWindow(640, 480, "Testing", nil, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	window.MakeContextCurrent()
+
+	for !window.ShouldClose() {
+		// Do OpenGL stuff.
+		window.SwapBuffers()
+		glfw.PollEvents()
+	}
+
+	// Important! Call gl.Init only under the presence of an active OpenGL context,
+	// i.e., after MakeContextCurrent.
+	if err := gl.Init(); err != nil {
+		log.Fatalln(err)
+	}
+}
